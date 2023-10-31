2b357fa71690cc9d272548a0ed39d73d56079d64
RockeyWereWolf <mi71k9d7p@code.edu.az>
2023-10-31 22:35:37 +0400

    uploaded  project

---
+++ StaticExtensionTask/StaticExtensionTask/Entities/Student.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExtensionTask.Entities
+{
+    internal class Student
+    {
+        static uint _id = 1;
+        public uint Id { get; }
+        public string Fullname { get; set; }
+        public string GroupNo { get; set; }
+        public byte Age { get; set; }
+
+        public static bool CheckGroupNo(string groupNo)
+        {
+            if (Char.IsUpper(groupNo[0]) && (groupNo.Substring(1).Length==3 && Convert.ToInt32(groupNo.Substring(1)) > 0)) 
+                return true;
+            return false;
+        }
+
+        public static bool CheckFullname(string fullname)
+        {
+            if (!fullname.Contains(" ")) 
+                return false;
+            string name = fullname.Split(' ')[0];
+            string surname = fullname.Split(' ')[1];
+            if (Char.IsUpper(name[0]) && Char.IsUpper(surname[0]))
+                return true;
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return $"Student {Id}. Fullname: {Fullname}, Group Number: {GroupNo}, Age: {Age}";
+        }
+
+        public Student(string fullname, string groupNo, byte age)
+        {
+            Fullname = fullname;
+            GroupNo = groupNo;
+            Age = age;
+            Id = _id;
+            _id++;
+        }
+    }
+}
---
+++ StaticExtensionTask/StaticExtensionTask/Program.cs
@@ -0,0 +1,48 @@
+﻿using StaticExtensionTask.Entities;
+using System.Text.RegularExpressions;
+
+namespace StaticExtensionTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student[] students = new Student[1];
+            bool createStudent = true;
+            do
+            {
+            Fullname:
+                Console.WriteLine("Enter fullname(Name Surname):");
+                string fullname = Console.ReadLine();
+                if (!Student.CheckFullname(fullname))
+                {
+                    Console.WriteLine("Fullname is not valid!");
+                    goto Fullname;
+                }
+            GroupNo:
+                Console.WriteLine("Group Number (one capital letter and 3 digit natural number):");
+                string groupNo = Console.ReadLine();
+                if (!Student.CheckGroupNo(groupNo))
+                {
+                    Console.WriteLine("Group Number is not valid!");
+                    goto GroupNo;
+                }
+                Console.WriteLine("Enter age:");
+                byte age = Convert.ToByte(Console.ReadLine());
+                students[students.Length - 1] = new Student(fullname, groupNo, age);
+                Console.WriteLine("All students: ");
+                foreach (var student in students) {
+                    Console.WriteLine(student);
+                }
+                Console.WriteLine("Create next student?(Yes/Y)");
+                string resonse = Console.ReadLine().ToLower();
+                if (!(resonse == "yes" || resonse == "y"))
+                {
+                    createStudent = false;
+                    continue;
+                }
+                Array.Resize(ref students, students.Length + 1);
+            } while (createStudent);
+        }
+    }
+}
